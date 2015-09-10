09c060b1d6bf2b9f3092c925ef53a29e963318c5
N0S3NS3 <zachary.moriarty@gmail.com>
2015-09-10 17:20:54 -0400

    added export into index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Modal = require('./src/Modal');
 exports.ConfirmModal = require('./src/ConfirmModal');
 exports.AlertModal = require('./src/AlertModal');
+exports.iframeModal = require('./src/iframeModal');
